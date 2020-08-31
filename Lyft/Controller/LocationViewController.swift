e236cc4e5675104e288d7e3b6795f69d2178f805
Weiran Fu <fuweiran816@gmail.com>
2020-08-31 16:18:08 -0400

    add auto-completion search in tableView

--- Lyft/Controller/LocationViewController.swift
+++ Lyft/Controller/LocationViewController.swift
@@ -7,27 +7,60 @@
 //
 
 import UIKit
+import MapKit
 
-class LocationViewController: UIViewController, UITableViewDataSource {
+class LocationViewController: UIViewController, UITableViewDataSource, UITextFieldDelegate, MKLocalSearchCompleterDelegate {
+    
+    @IBOutlet weak var dropoffTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     
     var locations = [Location]()
     var pickupLocation: Location?
     var dropoffLocation: Location?
     
+    var searchCompleter = MKLocalSearchCompleter()
+    var searchResults = [MKLocalSearchCompletion]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         locations = LocationService.shared.getRecentLocations()
+        
+        // Focus on dropoffTextField
+        dropoffTextField.becomeFirstResponder()
+        dropoffTextField.delegate = self
+        
+        // Delegate searchCompleter
+        searchCompleter.delegate = self
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let latestString = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        if latestString.count > 3 {
+            searchCompleter.queryFragment = latestString
+        }
+        return true
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return locations.count
+        return searchResults.isEmpty ? locations.count : searchResults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell") as! LocationCell
-        let location = locations[indexPath.row]
-        cell.update(location: location)
+        if searchResults.isEmpty {
+            let location = locations[indexPath.row]
+            cell.update(location: location)
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell.update(searchResult: searchResult)
+        }
         return cell
+    }
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+        // reload table view
+        tableView.reloadData()
     }
 }
--- Lyft/View/LocationCell.swift
+++ Lyft/View/LocationCell.swift
@@ -7,10 +7,10 @@
 //
 
 import UIKit
+import MapKit
 
 class LocationCell: UITableViewCell {
 
-    
     @IBOutlet weak var addressLine1Label: UILabel!
     @IBOutlet weak var addressLine2Label: UILabel!
     
@@ -18,5 +18,10 @@ class LocationCell: UITableViewCell {
     func update(location: Location) {
         addressLine1Label.text = location.title
         addressLine2Label.text = location.subtitle
+    }
+    
+    func update(searchResult: MKLocalSearchCompletion) {
+        addressLine1Label.text = searchResult.title
+        addressLine2Label.text = searchResult.subtitle
     }
 }
