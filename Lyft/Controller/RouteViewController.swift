2646605c9941384f67596a81bc20bb9427c24897
Weiran Fu <fuweiran816@gmail.com>
2020-08-31 18:52:53 -0400

    add cornerRadius to frames

---
+++ Lyft/Controller/RouteViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RouteViewController.swift
+//  Lyft
+//
+//  Created by Weiran Fu on 8/31/20.
+//  Copyright © 2020 aranne. All rights reserved.
+//
+
+import UIKit
+
+class RouteViewController: UIViewController {
+    
+    @IBOutlet weak var routeLabelContainer: UIView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var selectRideButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        routeLabelContainer.layer.cornerRadius = 10.0
+        backButton.layer.cornerRadius = backButton.frame.size.width / 2.0
+        selectRideButton.layer.cornerRadius = 10.0
+    }
+}
