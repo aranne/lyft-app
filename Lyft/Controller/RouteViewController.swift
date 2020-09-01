ce627774143ef3dfbbd3fd0b34d84001eca84484
Weiran Fu <fuweiran816@gmail.com>
2020-09-01 00:23:10 -0400

    add routeView in storyboard

--- Lyft/Controller/RouteViewController.swift
+++ Lyft/Controller/RouteViewController.swift
@@ -8,17 +8,39 @@
 
 import UIKit
 
-class RouteViewController: UIViewController {
+class RouteViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var routeLabelContainer: UIView!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var selectRideButton: UIButton!
     
+    var pickupLocation: Location?
+    var dropoffLocation: Location?
+    var rideQuotes = [RideQuote]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Rounding all the corners
         routeLabelContainer.layer.cornerRadius = 10.0
         backButton.layer.cornerRadius = backButton.frame.size.width / 2.0
         selectRideButton.layer.cornerRadius = 10.0
+        
+        let locations = LocationService.shared.getRecentLocations()
+        pickupLocation = locations[0]
+        dropoffLocation = locations[1]
+        
+        rideQuotes = RideQuoteService.shared.getQuotes(pickupLocation: pickupLocation!, dropoffLocation: dropoffLocation!)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rideQuotes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RideQuoteCell") as! RideQuoteCell
+        let rideQuote = rideQuotes[indexPath.row]
+        cell.update(rideQuote: rideQuote)
+        return cell
     }
 }
