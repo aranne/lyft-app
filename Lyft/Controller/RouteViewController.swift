9b77c859aedc5fdc99ae5727265e0ac6d5d2a5ee
Weiran Fu <fuweiran816@gmail.com>
2020-09-01 02:17:28 -0400

    add routes

--- Lyft/Controller/RouteViewController.swift
+++ Lyft/Controller/RouteViewController.swift
@@ -43,6 +43,9 @@ class RouteViewController: UIViewController, UITableViewDataSource, UITableViewD
         mapView.addAnnotations([pickupAnnotation, dropoffAnnotation])
         // Customize annotation
         mapView.delegate = self
+        
+        // Display route
+        displayRoute(sourceLocation: pickupLocation!, destinationLocation: dropoffLocation!)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -78,5 +81,45 @@ class RouteViewController: UIViewController, UITableViewDataSource, UITableViewD
         let locationAnnotation = annotation as! LocationAnnotation
         annotationView!.image = UIImage(named: "dot-\(locationAnnotation.locationType)")
         return annotationView
+    }
+    
+    // cutomize renderer for polyline renderer
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.lineWidth = 5.0
+        renderer.strokeColor = UIColor(red: 247.0 / 255.0, green: 66.0 / 255.0, blue: 190.0 / 255.0, alpha: 1.0)
+        return renderer
+    }
+    
+    func displayRoute(sourceLocation: Location, destinationLocation: Location) {
+        let sourceCoordinate = CLLocationCoordinate2D(latitude: sourceLocation.latitude, longitude: sourceLocation.longitude)
+        let destinationCoordinate = CLLocationCoordinate2D(latitude: destinationLocation.latitude, longitude: destinationLocation.longitude)
+        let sourcePlaceMark = MKPlacemark(coordinate: sourceCoordinate)
+        let destinationPlaceMark = MKPlacemark(coordinate: destinationCoordinate)
+        
+        let directionRequest = MKDirections.Request()
+        directionRequest.source = MKMapItem(placemark: sourcePlaceMark)
+        directionRequest.destination = MKMapItem(placemark: destinationPlaceMark)
+        directionRequest.transportType = .automobile
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate() { (respond, error) in
+            if let error = error {
+                print("There's an error with calculating route \(error)")
+                return
+            }
+            guard let respond = respond else {
+                return
+            }
+            
+            // add route overlay
+            let route = respond.routes[0]  // get the first route
+            self.mapView.addOverlay(route.polyline, level: .aboveRoads)
+            
+            // zoom into the route
+            let EDGE_INSET: CGFloat = 80.0
+            let boundingMapRect = route.polyline.boundingMapRect
+            self.mapView.setVisibleMapRect(boundingMapRect, edgePadding: UIEdgeInsets(top: EDGE_INSET, left: EDGE_INSET, bottom: EDGE_INSET, right: EDGE_INSET), animated: true)
+        }
     }
 }
