b317764550498d97fadc0933d63091bdf1200b8a
Weiran Fu <fuweiran816@gmail.com>
2020-09-02 00:42:34 -0400

    add coordinate to driver model

--- Lyft/Model/Driver.swift
+++ Lyft/Model/Driver.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 class Driver {
     let name: String
@@ -14,12 +15,14 @@ class Driver {
     let licenseNumber: String
     let rating: Float
     let car: String
+    let coordinate: CLLocationCoordinate2D
     
-    init(name: String, thumbnail: String, licenseNumber: String, rating: Float, car: String) {
+    init(name: String, thumbnail: String, licenseNumber: String, rating: Float, car: String, coordinate: CLLocationCoordinate2D) {
         self.name = name
         self.thumbnail = thumbnail
         self.licenseNumber = licenseNumber
         self.rating = rating
         self.car = car
+        self.coordinate = coordinate
     }
 }
